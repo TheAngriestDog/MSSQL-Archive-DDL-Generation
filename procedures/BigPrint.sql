d6ea9abadc44d7587c3ca030d199cf9d00b08726
mmacdonald <37754975+TheAngriestDog@users.noreply.github.com>
2020-08-21 10:49:33 -0400

    Added printing procedure to handle generated DDL larger than 4000 char

---
+++ procedures/BigPrint.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE [dbo].[BigPrint]
+	@Varchar		NVARCHAR(MAX)
+AS
+BEGIN
+	DECLARE @Count INT = 0;
+	DECLARE @TotalPrints INT = CEILING(LEN(@varchar) / 4000) + 1;
+	DECLARE @Pos INT = 0;
+	DECLARE @SubStringLen INT;
+
+	WHILE @Count < @TotalPrints
+	BEGIN
+		SET @SubStringLen = 4000 - CHARINDEX(CHAR(10), REVERSE(SUBSTRING(@Varchar, @Pos, 4000)));
+		PRINT SUBSTRING(@Varchar, @Pos, @SubStringLen);
+		SET @Count = @Count + 1;
+		SET @Pos = @Pos + @SubStringLen + 2;
+	END;
+END
